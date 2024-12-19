9c5f562b0cb08054c36ef80b030b6faf7e7f0dd1
Siavash Goudarzi <siavash.goudarzi@connectel.se>
2024-12-19 14:30:31 +0100

    array includes test

--- src/test/array.ts
+++ src/test/array.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert';
 import JsonJoi, { fromJson, toJson } from '../index';
 
 describe('Joi to Json - Array', () => {
@@ -11,6 +12,29 @@ describe('Joi to Json - Array', () => {
     const schema = fromJson(json_schema);
 
     schema.validate({ a: 'a', b: [1] });
+    done();
+  });
+
+  it('array includes', (done) => {
+    const original_schema = JsonJoi.object({
+      a: JsonJoi.string().when('b', {
+        is: JsonJoi.array().items(JsonJoi.string().valid("one").required(), JsonJoi.string()),
+        then: JsonJoi.string().required(),
+        otherwise: JsonJoi.string().forbidden(),
+      }),
+      b: JsonJoi.array().min(1)
+    });
+    const json_schema = toJson(original_schema);
+    const schema = fromJson(json_schema);
+
+    const result_success = schema.validate({ a: 'a', b: ["one"] });
+    const result_success2 = schema.validate({ a: 'a', b: ["one", "two"] });
+    const result_fail = schema.validate({ a: 'a', b: ["two"] });
+
+    assert.strictEqual(result_success.error, undefined);
+    assert.strictEqual(result_success2.error, undefined);
+    assert.strictEqual(!!result_fail.error, true);
+
     done();
   });
 
